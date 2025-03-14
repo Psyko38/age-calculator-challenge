032712af0bce7051869f54fd6cc8830c269f5953
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-03-14 14:43:58 +0100

    feat app : code work

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,29 @@ const runBTN = document.getElementById("calculateBtn");
 const dayIn = document.getElementById("dayIn");
 const monthIn = document.getElementById("monthIn");
 const yearIn = document.getElementById("yearIn");
+const yearOut = document.getElementById("yearOut");
+const monthOut = document.getElementById("monthOut");
+const dayOut = document.getElementById("dayOut");
 
 let ActualDate = "";
 runBTN.addEventListener("click", () => {
-	ActualDate = new Date();
-	HolDate = new Date("1018-09-22T00:00:00");
-	let aa = ActualDate.getTime() - HolDate.getTime();
-	console.log(HolDate.setTime(aa));
+	if (
+		yearIn.value >= 1970 &&
+		monthIn.value >= 1 &&
+		monthIn.value <= 12 &&
+		dayIn.value >= 1 &&
+		dayIn.value <= 31
+	) {
+		let DateNow = new Date();
+		let d1 = new Date();
+		let d2 = new Date(`${yearIn.value}/${monthIn.value}/${dayIn.value}`);
+		let diff = Math.abs(d1 - d2);
+		let r1 = new Date(d1.setTime(diff));
+		let Year = r1.getFullYear() - 1970;
+		let Month = r1.getMonth();
+		let Day = r1.getDay();
+		yearOut.innerHTML = Year;
+		monthOut.innerHTML = Month;
+		dayOut.innerHTML = Day;
+	}
 });

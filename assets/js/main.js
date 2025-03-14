fad70e5d90f4bed2e6a685dcb3e0e3948c313513
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-03-14 15:10:51 +0100

    Feat : Remov unless code

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,6 @@ const yearOut = document.getElementById("yearOut");
 const monthOut = document.getElementById("monthOut");
 const dayOut = document.getElementById("dayOut");
 
-let ActualDate = "";
 runBTN.addEventListener("click", () => {
 	if (
 		yearIn.value >= 1970 &&
@@ -15,16 +14,12 @@ runBTN.addEventListener("click", () => {
 		dayIn.value >= 1 &&
 		dayIn.value <= 31
 	) {
-		let DateNow = new Date();
 		let d1 = new Date();
 		let d2 = new Date(`${yearIn.value}/${monthIn.value}/${dayIn.value}`);
 		let diff = Math.abs(d1 - d2);
 		let r1 = new Date(d1.setTime(diff));
-		let Year = r1.getFullYear() - 1970;
-		let Month = r1.getMonth();
-		let Day = r1.getDay();
-		yearOut.innerHTML = Year;
-		monthOut.innerHTML = Month;
-		dayOut.innerHTML = Day;
+		yearOut.innerHTML = r1.getFullYear() - 1970;
+		monthOut.innerHTML = r1.getMonth();
+		dayOut.innerHTML = r1.getDay();
 	}
 });
